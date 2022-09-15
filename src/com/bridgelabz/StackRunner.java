071f6122c50cdc33b181abfa5eef79d3829d9bba
saps999 <saptarshibiswasgang@gmail.com>
2022-09-15 21:39:45 +0530

    Stack and Queue

--- src/com/bridgelabz/StackRunner.java
+++ src/com/bridgelabz/StackRunner.java
@@ -3,11 +3,11 @@ package com.bridgelabz;
 public class StackRunner {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.peek();
-        stack.push(20);
+        stack.push(70);
         stack.peek();
         stack.push(30);
+        stack.peek();
+        stack.push(56);
         stack.peek();
         stack.pop();
         stack.peek();
--- src/com/bridgelabz1/QueueRunner.java
+++ src/com/bridgelabz1/QueueRunner.java
@@ -3,9 +3,9 @@ package com.bridgelabz1;
 public class QueueRunner {
     public static void main(String[] args) {
         Queue<Integer> queue = new Queue<>();
-        queue.enqueue(10);
-        queue.enqueue(20);
+        queue.enqueue(70);
         queue.enqueue(30);
+        queue.enqueue(56);
         queue.enqueue(40);
         queue.print();
         queue.dequeue();
